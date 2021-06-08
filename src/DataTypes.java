d3a4df7c83a71e10d06c765fec181fc03645d596
rabiatul-adawiyah25 <rabiatuladawiyahhs@gmail.com>
2021-06-08 21:42:55 +0800

    deklarasi variabel dan tipe data

---
+++ src/DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        String namaDepan = "Rabiatul";
+        String namaBelakang = "Adawiyah";
+        int usia = 23;
+        int targetTahunKuliah = 4;
+        double ipk = 3.389764512;
+        char nilaiAbjad = 'A';
+        boolean cantik = true;
+
+    }
+}
