d8e6e2215d2cce0dcadc5b2476d80d51205a83c0
rabiatul-adawiyah25 <rabiatuladawiyahhs@gmail.com>
2021-06-08 21:52:44 +0800

    Operasi Tipe String

--- Modul01_IDEA/src/Main.java
+++ Modul01_IDEA/src/Main.java
@@ -14,5 +14,21 @@ public class Main {
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
 
+        String namaLengkap = namaDepan + " " + namaBelakang;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(5, 12));
+        System.out.println(namaLengkap.replace("Rab", "Badr"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split(" ");
+
+        for (String nama : namaArray) {
+            System.out.println(nama);
+        }
+
     }
 }
