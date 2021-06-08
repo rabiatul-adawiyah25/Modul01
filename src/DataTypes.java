6597548c9e1587bea45740f8b75c4a8d7737dc48
rabiatul-adawiyah25 <rabiatuladawiyahhs@gmail.com>
2021-06-08 21:46:35 +0800

    Input dengan Scanner

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,7 +1,10 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class DataTypes {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
 
         String namaDepan = "Rabiatul";
         String namaBelakang = "Adawiyah";
@@ -11,10 +14,23 @@ public class DataTypes {
         char nilaiAbjad = 'A';
         boolean cantik = true;
 
+        System.out.print("Input Nama Depan : ");
+        namaDepan = scanner.nextLine();
+        System.out.print("Input Usia : ");
+        usia = scanner.nextInt();
+        System.out.print("Input IPK : ");
+        ipk = scanner.nextDouble();
+        System.out.print("");
+        System.out.print("Input Nilai Abjad : ");
+        nilaiAbjad = scanner.next().charAt(0);
+        System.out.print("Cantik? : ");
+        cantik = scanner.nextBoolean();
+
+        System.out.println("============ OUTPUT ============");
         System.out.println("Nama depan : " + namaDepan);
         System.out.println("Nama belakang : " + namaBelakang);
         System.out.println("Usia : " + usia);
-        System.out.println("Target Kuliah : " + targetTahunKuliah);
+        System.out.println("Target Kuliah : " + targetTahunKuliah +" tahun");
         System.out.println("IPK : " + ipk);
         System.out.println("Nilai PBO : " + nilaiAbjad);
         System.err.println("Cantik : " + cantik);
