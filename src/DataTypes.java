0d932bba34ffb176ae8c40f72c6ec9bd53a6112b
rabiatul-adawiyah25 <rabiatuladawiyahhs@gmail.com>
2021-06-08 21:43:50 +0800

    Output dengan sout

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -9,5 +9,13 @@ public class DataTypes {
         char nilaiAbjad = 'A';
         boolean cantik = true;
 
+        System.out.println("Nama depan : " + namaDepan);
+        System.out.println("Nama belakang : " + namaBelakang);
+        System.out.println("Usia : " + usia);
+        System.out.println("Target Kuliah : " + targetTahunKuliah);
+        System.out.println("IPK : " + ipk);
+        System.out.println("Nilai PBO : " + nilaiAbjad);
+        System.err.println("Cantik : " + cantik);
+
     }
 }
